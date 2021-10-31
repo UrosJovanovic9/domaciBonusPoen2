35798997dc11c9369d0530be16f3fab676da366b
UrosJovanovic9 <urosh.jovanovicc@gmail.com>
2021-10-31 22:23:32 +0100

    Izmena

--- handler/update.php
+++ handler/update.php
@@ -19,18 +19,6 @@ require "../model/prijava.php";
 
 
 
-// if(isset($_POST['katedra']) && isset($_POST['predmet']) 
-// && isset($_POST['sala']) && isset($_POST['datum'])){
-//     $prijava = new Prijava(null,$_POST['predmet'],$_POST['katedra'],$_POST['sala'],$_POST['datum'] );
-//     $status = Prijava::add($prijava, $conn);
-
-//     if($status){
-//         echo 'Success';
-//     }else{
-//         echo $status;
-//         echo "Failed";
-//     }
-// }
 
 
 
