bfcfb29b222b9f17e9ca2c96a046e276433c24e1
UrosJovanovic9 <urosh.jovanovicc@gmail.com>
2021-10-31 22:10:25 +0100

    first commit

--- handler/update.php
+++ handler/update.php
@@ -2,4 +2,64 @@
 
 // kreirati  uodate funkciju
 
+
+
+require "../dbBroker.php";
+require "../model/prijava.php";
+
+// if(isset($_POST['id'])){
+//     $obj = new Prijava($_POST['id']);
+//     $status = $obj->deleteById($conn);
+//     if ($status){
+//         echo "Success";
+//     }else{
+//         echo "Failed";
+//     }
+// }
+
+
+
+// if(isset($_POST['katedra']) && isset($_POST['predmet']) 
+// && isset($_POST['sala']) && isset($_POST['datum'])){
+//     $prijava = new Prijava(null,$_POST['predmet'],$_POST['katedra'],$_POST['sala'],$_POST['datum'] );
+//     $status = Prijava::add($prijava, $conn);
+
+//     if($status){
+//         echo 'Success';
+//     }else{
+//         echo $status;
+//         echo "Failed";
+//     }
+// }
+
+
+
+
+if(isset($_POST['id']) && isset($_POST['katedra']) && isset($_POST['predmet']) && isset($_POST['sala']) && isset($_POST['datum'])){
+    $obj = new Prijava($_POST['id'], $_POST['katedra'],$_POST['predmet'],$_POST['sala'],$_POST['datum']);
+    $status = $obj->update($_POST['id'],$conn);
+    if ($status){
+        echo "Success";
+    }else{
+        echo "Failed";
+    }
+}
+
+
+// if(isset($_POST['id'])){
+//     $obj = new Prijava($_POST['id']);
+//     $status = $obj->update($_POST['id'],$conn);
+//     if ($status){
+//         echo "Success";
+//     }else{
+//         echo "Failed";
+//     }
+// }
+
+
+
+
+
+
+
 ?> 
